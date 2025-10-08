29b1dceff48a78c95b4cd21fbc034cd9df141fd6
Luiz Simon <>
2025-10-07 22:24:03 -0300

    removido pasta build

--- Jogo/build/CMakeFiles/Source.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Source.
-# This may be replaced when dependencies are built.
--- Jogo/build/CMakeFiles/fc-tmp/glm/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for glm")
-
-block(SCOPE_FOR VARIABLES)
-
-include("D:/Trabalhos/Trabalhos_2025/2_semestre/PG/Jogo/Jogo/build/CMakeFiles/fc-tmp/glm/glm-gitupdate.cmake")
-
-endblock()
